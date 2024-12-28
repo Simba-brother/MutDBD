4cd06dd6bd8b4352303b4f497806c924e3390c77
Simba-brother <2196664077@qq.com>
2024-12-28 22:43:30 +0800

    go

--- codes/config.py
+++ codes/config.py
@@ -25,13 +25,13 @@ poisoned_rate = 0.05
 ###########################################################
 
 # 当前实验设置的数据集名字
-dataset_name = "GTSRB"
+dataset_name = "CIFAR10"
 # 数据集分类任务数量
-class_num = 43 #  CIFAR-10:10,GTSRB:43
+class_num = 10 #  CIFAR-10:10,GTSRB:43
 # 当前实验设置的模型名字
 model_name = "DenseNet"
 # 当前实验设置的攻击
-attack_name = "BadNets"
+attack_name = "WaNet"
 # GPU设备
 gpu_id = 0
 
--- codes/ourMethod/additional_analysis/mutated_models_performance.py
+++ codes/ourMethod/additional_analysis/mutated_models_performance.py
@@ -5,11 +5,11 @@ import os
 import torch
 import joblib
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import accuracy_score
 from codes import config
 from codes.common.eval_model import EvalModel
 from codes.scripts.dataset_constructor import *
-
-
 
 
 def get_mutated_models_eval_acc(mutated_models_eval_ans):
@@ -24,6 +24,30 @@ def get_mutated_models_eval_acc(mutated_models_eval_ans):
         avg = round(sum(data[ratio])/len(data[ratio]),4)
         ans[ratio] = avg
     return ans
+
+def get_mutated_models_eval_acc_v2():
+    data ={}
+    for rate in config.fine_mutation_rate_list:
+        # 加载变异模型评估结果(csv)
+        df = pd.read_csv(os.path.join(
+            config.exp_root_dir,
+            "EvalMutationToCSV",
+            config.dataset_name,
+            config.model_name,
+            config.attack_name,
+            str(rate),
+            "preLabel.csv"
+        ))
+        GT_label_list = list(df["GT_label"])
+        avg_acc = 0
+        for i in range(config.mutation_model_num*5):
+            model_col_name = f"model_{i}"
+            pred_label_list = list(df[model_col_name])
+            acc = accuracy_score(GT_label_list,pred_label_list)
+            avg_acc += acc
+        avg_acc = round(avg_acc/(config.mutation_model_num*5),4)
+        data[rate] = avg_acc
+    return data
 
 def draw_line(x_ticks_label,y_value_list, save_path):
     x_ticks = [0,2,4,6,8,10,12]
@@ -42,7 +66,9 @@ def draw_line(x_ticks_label,y_value_list, save_path):
     plt.show()
     plt.savefig(save_path,transparent=False,dpi=600)
 
-if __name__ == "__main__":
+
+
+def main_v1():
     # 加载backdoor data
     # 加载后门模型数据
     backdoor_data_path = os.path.join(
@@ -94,3 +120,50 @@ if __name__ == "__main__":
     save_path = os.path.join(save_dir,save_file_name)
     draw_line(x_ticks_label,y_value_list,save_path)
     print(f"save_path:{save_path}")
+
+def main_v2():
+    # 加载backdoor data
+    # 加载后门模型数据
+    backdoor_data_path = os.path.join(
+        config.exp_root_dir, 
+        "ATTACK", 
+        config.dataset_name, 
+        config.model_name, 
+        config.attack_name, 
+        "backdoor_data.pth")
+    backdoor_data = torch.load(backdoor_data_path, map_location="cpu")
+    backdoor_model = backdoor_data["backdoor_model"]
+    poisoned_trainset =backdoor_data["poisoned_trainset"]
+    # 评估一下original backdoor model在poisoned_trainset上的acc
+    device = torch.device(f"cuda:{config.gpu_id}")
+    em = EvalModel(backdoor_model,poisoned_trainset,device)
+    origin_acc = em.eval_acc()
+
+    mutated_models_acc_dict = get_mutated_models_eval_acc_v2()
+
+    mutation_rate_list = config.fine_mutation_rate_list
+    mutated_models_acc_list = []
+    for rate in config.fine_mutation_rate_list:
+        mutated_models_acc_list.append(mutated_models_acc_dict[rate])
+    x_ticks_label = [0]
+    x_ticks_label.extend(mutation_rate_list)
+    
+    y_value_list = [origin_acc]
+    y_value_list.extend(mutated_models_acc_list)
+    
+
+    save_dir = os.path.join(
+        config.exp_root_dir,
+        "Figures",
+        config.dataset_name,
+        config.model_name,
+        config.attack_name
+    )
+    os.makedirs(save_dir,exist_ok=True)
+    save_file_name = "mutated_models_acc.png"
+    save_path = os.path.join(save_dir,save_file_name)
+    draw_line(x_ticks_label,y_value_list,save_path)
+    print(f"save_path:{save_path}")
+    
+if __name__ == "__main__":
+    main_v1()
