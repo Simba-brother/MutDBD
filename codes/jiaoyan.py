21455a52807d8a913fe5d82e2aa80ce0b364fbd0
Simba-brother <2196664077@qq.com>
2025-01-13 15:16:59 +0800

    go

--- codes/jiaoyan.py
+++ codes/jiaoyan.py
@@ -6,8 +6,13 @@ import pandas as pd
 import torch
 from codes import config
 
-def main():
-    pass
+def check(poisoned_ids,df):
+    set_1 = set(poisoned_ids)
+    df_poisoned = df.loc[df["isPoisoned"]==True]
+    set_2 = set(list(df_poisoned["sampled_id"]))
+    if set_1 != set_2:
+        return "No pass"
+    return "pass"
 
 if __name__ == "__main__":
     # 加载后门数据
@@ -22,11 +27,7 @@ if __name__ == "__main__":
     backdoor_data = torch.load(backdoor_data_path, map_location="cpu")
     backdoor_model = backdoor_data["backdoor_model"]
     poisoned_trainset =backdoor_data["poisoned_trainset"]
-
-    
     poisoned_ids = backdoor_data["poisoned_ids"]
-    set_1 = set(poisoned_ids)
-    pass_flag = True
     # 加载preLabel csv
     for rate in config.fine_mutation_rate_list:
         preLabel_csv_path = os.path.join(
@@ -39,13 +40,6 @@ if __name__ == "__main__":
                 "preLabel.csv"
             )
         preLabel_df = pd.read_csv(preLabel_csv_path)
-        df_poisoned = preLabel_df.loc[preLabel_df["isPoisoned"]==True]
-        set_2 = set(list(df_poisoned["sampled_id"]))
-        if set_1 != set_2:
-            print(f"rate:{rate} error")
-            pass_flag = False
-            break
-    if pass_flag is True:
-        print("pass")
-    else:
-        print("No pass")
+        check_flag = check(poisoned_ids,preLabel_df)
+        if check_flag == "No pass":
+            print(f"rate:{rate}没通过测试")
