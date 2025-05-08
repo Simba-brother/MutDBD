f2a7d94502dc4c0ce09599cb9744e1c4d77a8501
Simba-brother <2196664077@qq.com>
2025-05-08 13:39:32 +0800

    go

--- codes/compare.py
+++ codes/compare.py
@@ -47,22 +47,22 @@ ASD = {
         "DenseNet":{
             "BadNets":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/BadNets/2025-02-17_18:39:21/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/BadNets/2025-05-05_15:31:35/ckpt/latest_model.pt",
                 "exp_3":""
             },
             "IAD":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/IAD/2025-02-17_18:41:23/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/IAD/2025-05-05_15:38:38/ckpt/latest_model.pt",
                 "exp_3":""
             },
             "Refool":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/Refool/2025-02-17_18:47:05/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/Refool/2025-05-05_17:11:44/ckpt/latest_model.pt",
                 "exp_3":""
             },
             "WaNet":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/WaNet/2025-02-17_18:59:49/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/CIFAR10/DenseNet/WaNet/2025-05-05_17:24:40/ckpt/latest_model.pt",
                 "exp_3":""
             }
         }
@@ -115,22 +115,22 @@ ASD = {
         "DenseNet":{
             "BadNets":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/BadNets/2025-02-17_20:32:50/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/BadNets/2025-05-05_20:52:45/ckpt/latest_model.pt",
                 "exp_3":""
             },
             "IAD":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/IAD/2025-02-17_20:35:23/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/IAD/2025-05-05_21:09:36/ckpt/latest_model.pt",
                 "exp_3":""
             },
             "Refool":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/Refool/2025-02-17_20:40:02/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/Refool/2025-05-05_21:47:49/ckpt/latest_model.pt",
                 "exp_3":""
             },
             "WaNet":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/WaNet/2025-02-17_20:44:25/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/GTSRB/DenseNet/WaNet/2025-05-06_12:35:00/ckpt/latest_model.pt",
                 "exp_3":""
             }
         }
@@ -149,12 +149,12 @@ ASD = {
             },
             "Refool":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/ImageNet2012_subset/ResNet18/Refool/2025-03-07_21:03:57/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/ImageNet2012_subset/ResNet18/Refool/2025-05-06_13:56:16/ckpt/latest_model.pt",
                 "exp_3":""
             },
             "WaNet":{
                 "exp_1":"/data/mml/backdoor_detect/experiments/ASD/ImageNet2012_subset/ResNet18/WaNet/2025-03-07_21:31:50/ckpt/latest_model.pt",
-                "exp_2":"",
+                "exp_2":"/data/mml/backdoor_detect/experiments/ASD/ImageNet2012_subset/ResNet18/WaNet/2025-05-06_14:48:29/ckpt/latest_model.pt",
                 "exp_3":""
             }
         },
@@ -367,3 +367,140 @@ Our = {
         }
     }
 }
+
+import os
+import torch
+from torch.utils.data import DataLoader,Subset,ConcatDataset
+from codes import config
+from codes.common.eval_model import EvalModel
+
+# cifar10
+from codes.poisoned_dataset.cifar10.BadNets.generator import gen_poisoned_dataset as cifar10_badNets_gen_poisoned_dataset
+from codes.poisoned_dataset.cifar10.IAD.generator import gen_poisoned_dataset as cifar10_IAD_gen_poisoned_dataset
+from codes.poisoned_dataset.cifar10.Refool.generator import gen_poisoned_dataset as cifar10_Refool_gen_poisoned_dataset
+from codes.poisoned_dataset.cifar10.WaNet.generator import gen_poisoned_dataset as cifar10_WaNet_gen_poisoned_dataset
+# gtsrb
+from codes.poisoned_dataset.gtsrb.BadNets.generator import gen_poisoned_dataset as gtsrb_badNets_gen_poisoned_dataset
+from codes.poisoned_dataset.gtsrb.IAD.generator import gen_poisoned_dataset as gtsrb_IAD_gen_poisoned_dataset
+from codes.poisoned_dataset.gtsrb.Refool.generator import gen_poisoned_dataset as gtsrb_Refool_gen_poisoned_dataset
+from codes.poisoned_dataset.gtsrb.WaNet.generator import gen_poisoned_dataset as gtsrb_WaNet_gen_poisoned_dataset
+
+# imagenet
+from codes.poisoned_dataset.imagenet_sub.BadNets.generator import gen_poisoned_dataset as imagenet_badNets_gen_poisoned_dataset
+from codes.poisoned_dataset.imagenet_sub.IAD.generator import gen_poisoned_dataset as imagenet_IAD_gen_poisoned_dataset
+from codes.poisoned_dataset.imagenet_sub.Refool.generator import gen_poisoned_dataset as imagenet_Refool_gen_poisoned_dataset
+from codes.poisoned_dataset.imagenet_sub.WaNet.generator import gen_poisoned_dataset as imagenet_WaNet_gen_poisoned_dataset
+
+# transform数据集
+from codes.transform_dataset import cifar10_BadNets, cifar10_IAD, cifar10_Refool, cifar10_WaNet
+from codes.transform_dataset import gtsrb_BadNets, gtsrb_IAD, gtsrb_Refool, gtsrb_WaNet
+from codes.transform_dataset import imagenet_BadNets, imagenet_IAD, imagenet_Refool, imagenet_WaNet
+
+
+
+def get_fresh_dataset(poisoned_ids,dataset_name,attack_name):
+    if dataset_name == "CIFAR10":
+        if attack_name == "BadNets":
+            poisoned_trainset = cifar10_badNets_gen_poisoned_dataset(poisoned_ids,"train")
+            clean_trainset, clean_testset = cifar10_BadNets()
+
+        elif attack_name == "IAD":
+            poisoned_trainset = cifar10_IAD_gen_poisoned_dataset(model_name, poisoned_ids,"train")
+            clean_trainset, _, clean_testset, _ = cifar10_IAD()
+        elif attack_name == "Refool":
+            poisoned_trainset = cifar10_Refool_gen_poisoned_dataset(poisoned_ids,"train")
+            clean_trainset, clean_testset = cifar10_Refool()
+        elif attack_name == "WaNet":
+            poisoned_trainset = cifar10_WaNet_gen_poisoned_dataset(model_name,poisoned_ids,"train")
+            clean_trainset, clean_testset = cifar10_WaNet()
+    elif dataset_name == "GTSRB":
+        if attack_name == "BadNets":
+            poisoned_trainset = gtsrb_badNets_gen_poisoned_dataset(poisoned_ids,"train")
+            clean_trainset, clean_testset = gtsrb_BadNets()
+        elif attack_name == "IAD":
+            poisoned_trainset = gtsrb_IAD_gen_poisoned_dataset(model_name,poisoned_ids,"train")
+            clean_trainset, _, clean_testset, _ = gtsrb_IAD()
+        elif attack_name == "Refool":
+            poisoned_trainset = gtsrb_Refool_gen_poisoned_dataset(poisoned_ids,"train")
+            clean_trainset, clean_testset = gtsrb_Refool()
+        elif attack_name == "WaNet":
+            poisoned_trainset = gtsrb_WaNet_gen_poisoned_dataset(model_name, poisoned_ids,"train")
+            clean_trainset, clean_testset = gtsrb_WaNet()
+    elif dataset_name == "ImageNet2012_subset":
+        if attack_name == "BadNets":
+            poisoned_trainset = imagenet_badNets_gen_poisoned_dataset(poisoned_ids,"train")
+            clean_trainset, clean_testset = imagenet_BadNets()
+        elif attack_name == "IAD":
+            poisoned_trainset = imagenet_IAD_gen_poisoned_dataset(model_name,poisoned_ids,"train")
+            clean_trainset, _, clean_testset, _ = imagenet_IAD()
+        elif attack_name == "Refool":
+            poisoned_trainset = imagenet_Refool_gen_poisoned_dataset(poisoned_ids,"train")
+            clean_trainset, clean_testset = imagenet_Refool()
+        elif attack_name == "WaNet":
+            poisoned_trainset = imagenet_WaNet_gen_poisoned_dataset(model_name, poisoned_ids,"train")
+            clean_trainset, clean_testset = imagenet_WaNet()
+    return poisoned_trainset, clean_trainset, clean_testset
+
+
+
+
+
+dataset_name = "CIFAR10"
+model_name = "ResNet18"
+attack_name = "BadNets"
+exp_id = "exp_1"
+gpu_id = 0
+
+
+# 加载后门攻击配套数据
+backdoor_data_path = os.path.join(config.exp_root_dir, 
+                                "ATTACK",
+                                dataset_name,
+                                model_name,
+                                attack_name,
+                                "backdoor_data.pth")
+backdoor_data = torch.load(backdoor_data_path,map_location="cpu")
+# 后门模型
+backdoor_model = backdoor_data["backdoor_model"]
+backdoor_model.load_state_dict(torch.load(ASD[dataset_name][model_name][attack_name][exp_id],map_location="cpu"))
+# 训练数据集中中毒样本id
+poisoned_ids = backdoor_data["poisoned_ids"]
+# 预制的poisoned_testset
+poisoned_testset = backdoor_data["poisoned_testset"]
+# 根据poisoned_ids得到非预制菜poisoneds_trainset和新鲜clean_testset
+poisoned_trainset, clean_trainset, clean_testset = get_fresh_dataset(poisoned_ids,dataset_name,attack_name)
+# 从poisoned_testset中剔除原来就是target class的数据
+# 不打乱
+clean_testset_loader = DataLoader(
+            clean_testset, # 非预制
+            batch_size=64, 
+            shuffle=False,
+            num_workers=4,
+            pin_memory=True)
+clean_testset_label_list = []
+for _, batch in enumerate(clean_testset_loader):
+    Y = batch[1]
+    clean_testset_label_list.extend(Y.tolist())
+filtered_ids = []
+for sample_id in range(len(clean_testset)):
+    sample_label = clean_testset_label_list[sample_id]
+    if sample_label != config.target_class_idx:
+        filtered_ids.append(sample_id)
+filtered_poisoned_testset = Subset(poisoned_testset,filtered_ids)
+# 获得设备
+device = torch.device(f"cuda:{gpu_id}")
+e = EvalModel(backdoor_model,filtered_poisoned_testset,device)
+asr = e.eval_acc()
+print("ASR:",asr)
+e = EvalModel(backdoor_model,clean_testset,device)
+acc = e.eval_acc()
+print("ACC:",acc)
+print("="*30)
+print("总结")
+print("="*30)
+print(f"dataset_name:{dataset_name}")
+print(f"model_name:{model_name}")
+print(f"attack_name:{attack_name}")
+print(f"exp_id:{exp_id}")
+print(f"ASR:{asr}")
+print(f"ACC:{acc}")
