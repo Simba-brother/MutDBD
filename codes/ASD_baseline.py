6b67a2efae5f066b51b48462f818e11d1f7df3a6
Simba-brother <2196664077@qq.com>
2025-06-13 23:52:28 +0800

    go

--- codes/ASD_baseline.py
+++ codes/ASD_baseline.py
@@ -357,7 +357,7 @@ if __name__ == "__main__":
 
     gpu_id = 1
     baseline_name = "ASD_new"
-    rand_seed = 9
+    rand_seed = 1
     dataset_name = "ImageNet2012_subset"
     class_num = get_classNum(dataset_name)
     model_name = "ResNet18"
