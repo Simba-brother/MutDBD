a6d4222dfade3e9f9cc99a8842139d91d1d4c22e
Simba-brother <2196664077@qq.com>
2025-06-18 23:32:41 +0800

    go

--- codes/ASD_baseline.py
+++ codes/ASD_baseline.py
@@ -355,13 +355,13 @@ if __name__ == "__main__":
     # class_num = get_classNum(dataset_name)
     # main()
 
-    gpu_id = 0
+    gpu_id = 1
     baseline_name = "ASD_new"
-    rand_seed = 7
+    rand_seed = 4
     dataset_name = "ImageNet2012_subset"
     class_num = get_classNum(dataset_name)
     model_name = "ResNet18"
-    for attack_name in ["BadNets", "IAD", "Refool", "WaNet"]:
+    for attack_name in ["IAD"]:
         main()
 
     # for rand_seed in [10]:
