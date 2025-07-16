9118edcd02a68613464ce6e22c4678f969956893
Simba-brother <2196664077@qq.com>
2025-07-16 21:17:34 +0800

    go

--- codes/datasets/cifar10/attacks/LabelConsistent/main.py
+++ codes/datasets/cifar10/attacks/LabelConsistent/main.py
@@ -142,10 +142,10 @@ def bengin_main(model,trainset,testset):
 def attack_main(model,trainset,testset):    
     poisoned_rate = 0.1
     adv_model = copy.deepcopy(model)
-    benign_state_dict_path = os.path.join(exp_root_dir,"ATTACK",dataset_name, model_name, attack_name, "benign_train_2025-07-16_13:17:28", "best_model.pth")
+    benign_state_dict_path = os.path.join(exp_root_dir,"ATTACK",dataset_name, model_name, attack_name, "benign_train_2025-07-16_17:35:57", "best_model.pth")
     benign_state_dict = torch.load(benign_state_dict_path, map_location="cpu")
     adv_model.load_state_dict(benign_state_dict)
-    adv_dataset_dir = os.path.join(exp_root_dir,"ATTACK", dataset_name, model_name, attack_name, "adv_dataset"),
+    adv_dataset_dir = os.path.join(exp_root_dir,"ATTACK", dataset_name, model_name, attack_name, "adv_dataset")
     attacker = get_attacker(trainset,testset,model,target_class,poisoned_rate,
                             adv_model,adv_dataset_dir)
     attacker.train()
@@ -187,7 +187,7 @@ if __name__ == "__main__":
     global_seed = 0
     torch.manual_seed(global_seed) # cpu随机数种子
     deterministic = True
-    is_benign = True
+    is_benign = False
     experiment_name = "benign_train" if is_benign else "attack_train"
     schedule = {
         'device': f'cuda:{gpu_id}',
