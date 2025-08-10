603ada9599acb9726b88a5e0a3c0973b37d2f197
Simba-brother <2196664077@qq.com>
2025-08-10 16:49:48 +0800

    go

--- codes/ourMethod/mutation_model_eval.py
+++ codes/ourMethod/mutation_model_eval.py
@@ -12,23 +12,7 @@ import joblib
 
 from codes.scripts.dataset_constructor import *
 from codes.common.eval_model import EvalModel
-# cifar10
-from codes.poisoned_dataset.cifar10.BadNets.generator import gen_poisoned_dataset as cifar10_badNets_gen_poisoned_dataset
-from codes.poisoned_dataset.cifar10.IAD.generator import gen_poisoned_dataset as cifar10_IAD_gen_poisoned_dataset
-from codes.poisoned_dataset.cifar10.Refool.generator import gen_poisoned_dataset as cifar10_Refool_gen_poisoned_dataset
-from codes.poisoned_dataset.cifar10.WaNet.generator import gen_poisoned_dataset as cifar10_WaNet_gen_poisoned_dataset
-# gtsrb
-from codes.poisoned_dataset.gtsrb.BadNets.generator import gen_poisoned_dataset as gtsrb_badNets_gen_poisoned_dataset
-from codes.poisoned_dataset.gtsrb.IAD.generator import gen_poisoned_dataset as gtsrb_IAD_gen_poisoned_dataset
-from codes.poisoned_dataset.gtsrb.Refool.generator import gen_poisoned_dataset as gtsrb_Refool_gen_poisoned_dataset
-from codes.poisoned_dataset.gtsrb.WaNet.generator import gen_poisoned_dataset as gtsrb_WaNet_gen_poisoned_dataset
-
-# imagenet
-from codes.poisoned_dataset.imagenet_sub.BadNets.generator import gen_poisoned_dataset as imagenet_badNets_gen_poisoned_dataset
-from codes.poisoned_dataset.imagenet_sub.IAD.generator import gen_poisoned_dataset as imagenet_IAD_gen_poisoned_dataset
-from codes.poisoned_dataset.imagenet_sub.Refool.generator import gen_poisoned_dataset as imagenet_Refool_gen_poisoned_dataset
-from codes.poisoned_dataset.imagenet_sub.WaNet.generator import gen_poisoned_dataset as imagenet_WaNet_gen_poisoned_dataset
-
+from codes.bigUtils.dataset import get_all_dataset
 
 
 def get_mutation_models_pred_labels(dataset):
@@ -258,56 +242,24 @@ def main(exp_sub,save_format):
             save_file_path = os.path.join(save_dir,save_file_name)
             ansToParquet(data_dict,save_file_path)
 
-
-def get_poisoned_trainset(poisoned_ids):
-    if dataset_name == "CIFAR10":
-        if attack_name == "BadNets":
-            poisoned_trainset = cifar10_badNets_gen_poisoned_dataset(poisoned_ids,"train")
-        elif attack_name == "IAD":
-            poisoned_trainset = cifar10_IAD_gen_poisoned_dataset(model_name, poisoned_ids,"train")
-        elif attack_name == "Refool":
-            poisoned_trainset = cifar10_Refool_gen_poisoned_dataset(poisoned_ids,"train")
-        elif attack_name == "WaNet":
-            poisoned_trainset = cifar10_WaNet_gen_poisoned_dataset(model_name,poisoned_ids,"train")
-    elif dataset_name == "GTSRB":
-        if attack_name == "BadNets":
-            poisoned_trainset = gtsrb_badNets_gen_poisoned_dataset(poisoned_ids,"train")
-        elif attack_name == "IAD":
-            poisoned_trainset = gtsrb_IAD_gen_poisoned_dataset(model_name,poisoned_ids,"train")
-        elif attack_name == "Refool":
-            poisoned_trainset = gtsrb_Refool_gen_poisoned_dataset(poisoned_ids,"train")
-        elif attack_name == "WaNet":
-            poisoned_trainset = gtsrb_WaNet_gen_poisoned_dataset(model_name, poisoned_ids,"train")
-    elif dataset_name == "ImageNet2012_subset":
-        if attack_name == "BadNets":
-            poisoned_trainset = imagenet_badNets_gen_poisoned_dataset(poisoned_ids,"train")
-        elif attack_name == "IAD":
-            poisoned_trainset = imagenet_IAD_gen_poisoned_dataset(model_name,poisoned_ids,"train")
-        elif attack_name == "Refool":
-            poisoned_trainset = imagenet_Refool_gen_poisoned_dataset(poisoned_ids,"train")
-        elif attack_name == "WaNet":
-            poisoned_trainset = imagenet_WaNet_gen_poisoned_dataset(model_name, poisoned_ids,"train")
-    return poisoned_trainset
-
-
 if __name__ == "__main__":
     # 进程名称
     exp_root_dir = "/data/mml/backdoor_detect/experiments/"
     dataset_name = "CIFAR10"
-    model_name = "ResNet18"
+    model_name = "DenseNet"
     attack_name = "BadNets"
-    main_exp_name = "EvalMutationToCSV_ForDiscussion" 
+    main_exp_name = "EvalMutationToCSV" # "EvalMutationToCSV_ForDiscussion" 
     sub_exp_name = "preLabel"
-    mutation_rate_list = [0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9] # [0.03, 0.05, 0.07, 0.09, 0.1]
+    mutation_rate_list = [0.03, 0.05, 0.07, 0.09, 0.1], # [0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9] 
     mutation_name_list = ["Gaussian_Fuzzing","Weight_Shuffling","Neuron_Activation_Inverse","Neuron_Block","Neuron_Switch"]
-    mutation_model_num = 10
+    mutation_model_num = 50
     proctitle = f"{main_exp_name}|{dataset_name}|{model_name}|{attack_name}"
     setproctitle.setproctitle(proctitle)
     device = torch.device(f"cuda:1")
 
     # 日志保存目录
     LOG_FORMAT = "时间：%(asctime)s - 日志等级：%(levelname)s - 日志信息：%(message)s"
-    LOG_FILE_DIR = os.path.join("log","EvalMutationToCSV_ForDiscussion",dataset_name,model_name,attack_name)
+    LOG_FILE_DIR = os.path.join("log","EvalMutationToCSV",dataset_name,model_name,attack_name)
     os.makedirs(LOG_FILE_DIR,exist_ok=True)
     LOG_FILE_NAME = f"{main_exp_name}_{sub_exp_name}.log"
     LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
@@ -327,7 +279,7 @@ if __name__ == "__main__":
         backdoor_model = backdoor_data["backdoor_model"]
         backdoor_model_origin = copy.deepcopy(backdoor_model)
         poisoned_ids = backdoor_data["poisoned_ids"]
-        poisoned_trainset = get_poisoned_trainset(poisoned_ids)
+        poisoned_trainset, filtered_poisoned_testset, clean_trainset, clean_testset = get_all_dataset(dataset_name, model_name, attack_name, poisoned_ids)
         logging.debug(f"开始:得到所有变异模型在poisoned trainset上的预测{sub_exp_name}结果")
         main(f"{sub_exp_name}",save_format="csv")
         logging.debug("End")
